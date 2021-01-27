ebeac9b8462247caf2ea60d2361fcc68572ad227
cb <cb@cbdeiMac.local>
2021-01-27 19:01:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## auto creat macOS .icns file
+
+```
+bash -c "$(curl -fsSL https://github.com/ubun222/Aicns/main/icns.sh)" Sample.png
+
+
+```
+
+* You just need to drop your png file in the shell window.
+* The icns file is going to be created in the path same as the Sample.png.
---
+++ icns.sh
@@ -0,0 +1,33 @@
+index=$(dirname $1)
+
+cd $index
+
+target=$(echo $1| sed 's/$index//')
+
+mkdir icons.iconset 
+
+sips -z 16 16     $target --out icons.iconset/icon_16x16.png
+
+sips -z 32 32     $target --out icons.iconset/icon_16x16@2x.png
+
+sips -z 32 32     $target --out icons.iconset/icon_32x32.png
+
+sips -z 64 64     $target --out icons.iconset/icon_32x32@2x.png
+
+sips -z 64 64     $target --out icons.iconset/icon_64x64.png
+
+sips -z 128 128   $target --out icons.iconset/icon_64x64@2x.png
+
+sips -z 128 128   $target --out icons.iconset/icon_128x128.png
+
+sips -z 256 256   $target --out icons.iconset/icon_128x128@2x.png
+
+sips -z 256 256   $target --out icons.iconset/icon_256x256.png
+
+sips -z 512 512   $target --out icons.iconset/icon_256x256@2x.png
+
+sips -z 512 512   $target --out icons.iconset/icon_512x512.png
+
+sips -z 1024 1024   $target --out icons.iconset/icon_512x512@2x.png
+
+iconutil -c icns icons.iconset -o Icon.icns
