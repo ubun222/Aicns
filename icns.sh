4166815c21e91b172f51e6ba6ffdb872f5db44ff
cb <cb@cbdeiMac.local>
2021-01-27 19:51:04 +0800

    改成-zsh

--- icns.sh
+++ icns.sh
@@ -1,10 +1,8 @@
-p=$0
-
-index=$(dirname "${1:=p}")
+index=$(dirname "${0}")
 
 cd $index
 
-target=$(echo "${1:=p}"| sed 's/$index//')
+target=$(echo "${0}"| sed 's/$index//')
 
 mkdir icons.iconset 
 
